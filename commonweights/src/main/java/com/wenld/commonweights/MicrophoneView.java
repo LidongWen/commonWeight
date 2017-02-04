76cc78d4b3b58f223e0fdf7ed012ee7d250fc6a8
wenld <wenld2014@163.om>
2017-02-04 16:35:04 +0800

    继承CustomView

--- commonweights/src/main/java/com/wenld/commonweights/MicrophoneView.java
+++ commonweights/src/main/java/com/wenld/commonweights/MicrophoneView.java
@@ -14,7 +14,6 @@ import android.graphics.drawable.Drawable;
 import android.os.Handler;
 import android.os.Message;
 import android.util.AttributeSet;
-import android.view.View;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -25,10 +24,9 @@ import static android.R.attr.mode;
  * Created by wenld on 2017/1/12.
  */
 
-public class MicrophoneView extends View {
+public class MicrophoneView extends CustomView {
 
-    Context mContext;
-    private final TypedArray typedArray;
+    private TypedArray typedArray;
     public final static int MODEL_LOADING = 3;//标识中
     public final static int MODEL_PLAY = 2; //播放
     public final static int MODEL_RECORD = 1;//录制
@@ -70,6 +68,13 @@ public class MicrophoneView extends View {
      */
     private int volumeLevel = 2;
 
+
+    RectF[] rectFLoadings;
+    RectF[] rectFMicrophones;
+    RectF rectFBitmap;
+    RectF rectFRing;
+    RectF rectFLight;
+
     Bitmap bitmap;
 
 
@@ -107,18 +112,14 @@ public class MicrophoneView extends View {
 
     public MicrophoneView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        this.mContext = context;
-        typedArray = context.obtainStyledAttributes(attrs, R.styleable.recordView);
-        initAtts();
-
-        mPaint = new Paint();
-        paintText = new Paint(Paint.ANTI_ALIAS_FLAG);
     }
 
-    private void initAtts() {
+    @Override
+    public void initAttrs(AttributeSet attrs) {
+        typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.recordView);
         model = typedArray.getInt(R.styleable.recordView_model, MODEL_RECORD);
         playHintText = typedArray.getString(R.styleable.recordView_playHintText);
-        textHintSize = typedArray.getDimension(R.styleable.recordView_hintTextSize, dip2px(mContext, 15));
+        textHintSize = typedArray.getDimension(R.styleable.recordView_hintTextSize, dip2px(15));
         textHintColor = typedArray.getColor(R.styleable.recordView_textHintColor, Color.parseColor("#45C3E5"));
 
         ringColor = typedArray.getColor(R.styleable.recordView_ringColor, Color.parseColor("#DAF6FE"));
@@ -128,9 +129,9 @@ public class MicrophoneView extends View {
         voiceRingColor = typedArray.getColor(R.styleable.recordView_voiceRingColor, Color.parseColor("#45C3E5"));
         converColor();
 
-        ringWidth = typedArray.getDimension(R.styleable.recordView_ringWidth, dip2px(mContext, 3));
+        ringWidth = typedArray.getDimension(R.styleable.recordView_ringWidth, dip2px(3));
 
-        voiceWidth = typedArray.getDimension(R.styleable.recordView_voiceWidth, dip2px(mContext, 1));
+        voiceWidth = typedArray.getDimension(R.styleable.recordView_voiceWidth, dip2px(1));
 
         Drawable drawable = typedArray.getDrawable(R.styleable.recordView_microphoneBitmap);
         if (drawable != null) {
@@ -139,40 +140,54 @@ public class MicrophoneView extends View {
             bitmap = BitmapFactory.decodeResource(getResources(),
                     R.drawable.ic_microphone);
         }
+        typedArray.recycle();
     }
 
-    /**
-     * 当布局为wrap_content时设置默认长宽
-     *
-     * @param widthMeasureSpec
-     * @param heightMeasureSpec
-     */
     @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        setMeasuredDimension(measure(widthMeasureSpec), measure(heightMeasureSpec));
+    public void initValue() {
+        mPaint = new Paint();
+        paintText = new Paint(Paint.ANTI_ALIAS_FLAG);
     }
 
-    private int measure(int origin) {
-        int result = DEFAULT_MIN_WIDTH;
-        int specMode = MeasureSpec.getMode(origin);
-        int specSize = MeasureSpec.getSize(origin);
-        if (specMode == MeasureSpec.EXACTLY) {
-            result = specSize;
-        } else {
-            if (specMode == MeasureSpec.AT_MOST) {
-                result = Math.min(result, specSize);
-            }
+    @Override
+    public void reset() {
+        rectFLoadings = new RectF[]{};
+        for (int i = 0; i < 3; i++) {
+            RectF oval = new RectF(dip2px(pandding + 2 * i)
+                    , dip2px(pandding + 2 * i)
+                    , getWidth() - dip2px(pandding + 2 * i)
+                    , getHeight() - dip2px(pandding + 2 * i));
+            rectFLoadings[i] = oval;
         }
-        return result;
+
+        float w = getWidth() / 2 - dip2px(pandding + 5);
+        float h = getHeight() / 2 - dip2px(pandding + 5);
+        rectFBitmap = new RectF(getWidth() / 2 - w / 2
+                , getHeight() / 2
+                , w / 2 + getWidth() / 2
+                , getHeight() / 2 + h);
+
+
+        rectFRing = new RectF(dip2px(pandding)
+                , dip2px(pandding)
+                , getWidth() - dip2px(pandding)
+                , getHeight() - dip2px(pandding));
+
+        rectFMicrophones = new RectF[]{};
+
+        for (int i = 5; i > 0; i--) {
+            float currentR = circleR / 5 * (i);
+            RectF rectFArc = new RectF(getWidth() / 2 - currentR
+                    , getHeight() / 2 - currentR
+                    , getWidth() / 2 + currentR
+                    , getHeight() / 2 + currentR);
+            rectFMicrophones[i] = rectFArc;
+        }
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        int left = getPaddingLeft();
-        int top = getPaddingTop();
-        int right = getWidth() - getPaddingRight();
-        int bottom = getHeight() - getPaddingBottom();
 
         switch (model) {
             case MODEL_LOADING: {
@@ -196,14 +211,7 @@ public class MicrophoneView extends View {
      * @param canvas
      */
     private void onDrawMicrophone(Canvas canvas) {
-
-        float w = getWidth() / 2 - dip2px(mContext, pandding + 5);
-        float h = getHeight() / 2 - dip2px(mContext, pandding + 5);
-        RectF rectF = new RectF(getWidth() / 2 - w / 2
-                , getHeight() / 2
-                , w / 2 + getWidth() / 2
-                , getHeight() / 2 + h);
-        canvas.drawBitmap(bitmap, null, rectF, null);
+        canvas.drawBitmap(bitmap, null, rectFBitmap, null);
     }
 
     private void drawText(Canvas canvas) {
@@ -213,7 +221,7 @@ public class MicrophoneView extends View {
 
         paintText.setTextSize(textHintSize);
         paintText.setColor(textHintColor);
-        Paint.FontMetrics fm = mPaint.getFontMetrics();
+        Paint.FontMetrics fm = paintText.getFontMetrics();
         paintText.setTextAlign(Paint.Align.CENTER);
         if (playHintText == null) {
             playHintText = "正在播放录音.";
@@ -229,11 +237,7 @@ public class MicrophoneView extends View {
         mPaint.setStyle(Paint.Style.STROKE);
         mPaint.setStrokeWidth(ringWidth);
         mPaint.setColor(ringColor);
-        RectF oval = new RectF(dip2px(mContext, pandding)
-                , dip2px(mContext, pandding)
-                , getWidth() - dip2px(mContext, pandding)
-                , getHeight() - dip2px(mContext, pandding));
-        canvas.drawArc(oval, 0, 360, false, mPaint);    //绘制圆弧
+        canvas.drawArc(rectFRing, 0, 360, false, mPaint);    //绘制圆弧
     }
 
     /**
@@ -260,11 +264,8 @@ public class MicrophoneView extends View {
 //        }
 //        mPaint.setShader(new SweepGradient(getWidth() / 2, getHeight() / 2, doughnutColors, null));
         float length = (float) (45 * Math.sin(Math.toRadians(i * 60 + progress)) + 75);
-        RectF oval = new RectF(dip2px(mContext, pandding + 2 * i)
-                , dip2px(mContext, pandding + 2 * i)
-                , getWidth() - dip2px(mContext, pandding + 2 * i)
-                , getHeight() - dip2px(mContext, pandding + 2 * i));
-        canvas.drawArc(oval, (120 - length) + progress + i * 80, length, false, mPaint);    //绘制圆弧
+
+        canvas.drawArc(rectFLoadings[i], (120 - length) + progress + i * 80, length, false, mPaint);    //绘制圆弧
     }
 
     private void drawArc(Canvas canvas) {
@@ -273,21 +274,16 @@ public class MicrophoneView extends View {
 //        mPaint.setColor(getResources().getColor(R.color.RoundFillColor));
         mPaint.setStrokeWidth(voiceWidth);
         mPaint.setShader(new SweepGradient(getWidth() / 2, getHeight() / 2, doughnutColors, null));
-        float r = getWidth() / 2f - dip2px(mContext, pandding);
         for (int i = 5; i > 0; i--) {
-            drawArc(r, i, canvas);
+            drawArc(i, canvas);
         }
-        drawLightArc(r, volumeLevel, canvas);
+        drawLightArc(circleR, volumeLevel, canvas);
         mPaint.reset();
     }
 
-    private void drawArc(float r, int i, Canvas canvas) {
-        float currentR = r / 5 * (i);
-        RectF oval = new RectF(getWidth() / 2 - currentR
-                , getHeight() / 2 - currentR
-                , getWidth() / 2 + currentR
-                , getHeight() / 2 + currentR);
-        canvas.drawArc(oval, 160, 360, false, mPaint);    //绘制圆弧
+    private void drawArc(int i, Canvas canvas) {
+
+        canvas.drawArc(rectFMicrophones[i], 160, 360, false, mPaint);    //绘制圆弧
     }
 
     /**
@@ -303,19 +299,11 @@ public class MicrophoneView extends View {
         mPaint.setShader(new SweepGradient(getWidth() / 2, getHeight() / 2, voiceColor, voiceColor));
         float currentR = r / 20 * (i);
 
-        RectF oval = new RectF(getWidth() / 2 - currentR
+        rectFLight = new RectF(getWidth() / 2 - currentR
                 , getHeight() / 2 - currentR
                 , getWidth() / 2 + currentR
                 , getHeight() / 2 + currentR);
-        canvas.drawArc(oval, 160, 360, true, mPaint);    //绘制圆弧
-    }
-
-    /**
-     * 根据手机的分辨率从 dp 的单位 转成为 px(像素)
-     */
-    public static float dip2px(Context context, float dpValue) {
-        final float scale = context.getResources().getDisplayMetrics().density;
-        return (dpValue * scale + 0.5f);
+        canvas.drawArc(rectFLight, 160, 360, true, mPaint);    //绘制圆弧
     }
 
     public void setModel(int model) {
